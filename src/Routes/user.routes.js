dc284de0a44ee4e819af2fcdf3764eb5c320d197
Arnav Srivastava <aranavsrivastav25@gmail.com>
2024-12-18 21:00:13 +0530

    added the routes in user route

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken} from '../Controllers/user.controller.js'
+import {registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserImage, 
+    getUserChannelProfile,
+     getWatchHistory} from '../Controllers/user.controller.js'
 import {upload} from "../MiddleWare/multer.middleware.js"
 import {verifyJWT} from "../MiddleWare/auth.middleware.js"
 
@@ -26,6 +36,15 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").post(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails )
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserImage)
+//when we are req.params then we are making access to the url so we ahve to encode the url 
+//we are passing in the url 
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 
